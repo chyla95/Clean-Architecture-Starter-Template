047cf65dc09ea4cc180555bda9ef978fb5454989
chyla95 <chyla95@gmail.com>
2023-12-02 21:54:38 +0100

    Clean up the api

---
+++ src/Sample.Api/Sample.Api.Public/Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Api.Public.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<bool> Get() => Ok(true);
+    }
+}
