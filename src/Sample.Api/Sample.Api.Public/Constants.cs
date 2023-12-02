7cb4cab63e3a11cca9ac08e1c783e2926074726a
chyla95 <chyla95@gmail.com>
2023-12-03 00:13:57 +0100

    Improve settings naming

--- src/Sample.Api/Sample.Api.Public/Constants.cs
+++ src/Sample.Api/Sample.Api.Public/Constants.cs
@@ -5,6 +5,6 @@ public static class Constants
     public static class AppSettingsKeys
     {
         public const string DatabaseConnectionString = "ConnectionStrings:Database";
-        public const string JwtSecret = "Jwt:Secret";
+        public const string JwtSecret = "JwtSettings:Secret";
     }
 }
