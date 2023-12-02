09afaf60009d1099e6afbe65c674e11f45eeca78
chyla95 <chyla95@gmail.com>
2023-12-02 22:47:40 +0100

    Add basic accessors

---
+++ src/Sample.Api/Sample.Api.Common/Accessors/IApplicationLifetimeAccessor.cs
@@ -0,0 +1,8 @@
+﻿namespace MC.Sample.Api.Utilities.Accessors;
+
+public interface IApplicationLifetimeAccessor
+{
+	CancellationToken ApplicationStartedCancellationToken { get; }
+	CancellationToken ApplicationStoppedCancellationToken { get; }
+	CancellationToken ApplicationStoppingCancellationToken { get; }
+}
