12a01695514174ab267b8771fe48e3ba7fd4b770
chyla95 <chyla95@gmail.com>
2023-12-02 21:54:38 +0100

    Clean up the api

--- src/Sample.Api/Sample.Api.Public/Program.cs
+++ src/Sample.Api/Sample.Api.Public/Program.cs
@@ -1,25 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Services
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSwaggerGen();
+}
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+// Middlewares
+WebApplication app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
