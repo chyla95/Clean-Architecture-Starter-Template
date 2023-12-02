5cd7abefcc2c7cb5bad239b5dbbffb6152766f9e
chyla95 <chyla95@gmail.com>
2023-12-03 00:01:54 +0100

    Add entity base

---
+++ src/Sample.Architecture/Sample.Architecture.Domain/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Sample.Architecture.Domain.Entities;
+
+public class User : Entity
+{
+    public required string Email { get; set; }
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+}
