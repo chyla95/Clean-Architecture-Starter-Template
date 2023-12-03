e814cac0848d81f032bb665a3a8d40bfdd831501
chyla95 <chyla95@gmail.com>
2023-12-03 03:22:16 +0100

    add settings validation with FluentValidation package

---
+++ src/Sample.Api/Sample.Api.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Sample.Api.Common.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static OptionsBuilder<TOptions> ValidateSettings<TOptions, TValidator>(this IServiceCollection services, string configurationSection)
+    where TOptions : class
+    where TValidator : class, IValidator<TOptions>
+    {
+        services.AddScoped<IValidator<TOptions>, TValidator>();
+
+        return services.AddOptions<TOptions>()
+            .BindConfiguration(configurationSection)
+            .ValidateWithFluentValidation()
+            .ValidateOnStart();
+    }
+}
