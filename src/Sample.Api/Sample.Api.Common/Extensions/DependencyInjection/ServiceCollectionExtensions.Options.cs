346802add24b0270413a300efd2cfcade4e558ea
chyla95 <chyla95@gmail.com>
2023-12-30 19:13:38 +0100

    Fix options binding

--- src/Sample.Api/Sample.Api.Common/Extensions/DependencyInjection/ServiceCollectionExtensions.Options.cs
+++ src/Sample.Api/Sample.Api.Common/Extensions/DependencyInjection/ServiceCollectionExtensions.Options.cs
@@ -4,19 +4,13 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Sample.Api.Common.Extensions.DependencyInjection;
 public static partial class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddOptions<TOptions>(this IServiceCollection services, string sectionPath, Action<BinderOptions>? configureBinder = null)
-        where TOptions : class, new()
+    public static IServiceCollection AddOptionsWithBindings<TOptions>(this IServiceCollection services, string sectionPath, Action<BinderOptions>? configureBinder = null)
+        where TOptions : class
     {
         services
             .AddOptions<TOptions>()
             .BindConfiguration(sectionPath, configureBinder);
 
         return services;
-    }
-
-    public static IServiceCollection AddAndValidateOptions<TOptions>(this IServiceCollection services, string sectionPath, Action<BinderOptions>? configureBinder = null)
-        where TOptions : class, new()
-    {
-        throw new NotImplementedException();
     }
 }
