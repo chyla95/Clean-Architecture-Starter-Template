d1985a68863d790619cf21d89706fc4ad817c9f8
chyla95 <chyla95@gmail.com>
2023-12-31 03:14:53 +0100

    Add default AppSettingsKeys

--- src/Sample.Api.Authentication/Sample.Api.Authentication.Jwt/Sample.Api.Authentication.Jwt/Extensions/DependencyInjection/ServiceCollectionExtensions.JwtValidator.cs
+++ src/Sample.Api.Authentication/Sample.Api.Authentication.Jwt/Sample.Api.Authentication.Jwt/Extensions/DependencyInjection/ServiceCollectionExtensions.JwtValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Sample.Api.Authentication.Jwt.Constants;
 using Sample.Api.Authentication.Jwt.Options;
 using Sample.Api.Authentication.Jwt.Services;
 using Sample.Api.Authentication.Jwt.Strategies;
@@ -6,10 +7,10 @@ using Sample.Api.Authentication.Jwt.Strategies;
 namespace Sample.Api.Authentication.Jwt.Extensions.DependencyInjection;
 public static partial class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddJwtValidator<TSigningCredentialsCreationStrategy>(this IServiceCollection services, string jwtValidatorOptionsSectionName)
+    public static IServiceCollection AddJwtValidator<TSigningCredentialsCreationStrategy>(this IServiceCollection services)
         where TSigningCredentialsCreationStrategy : class, IJwtValidatorSigningCredentialsCreationStrategy
     {
-        services.AddOptions<JwtValidatorOptions>().BindConfiguration(jwtValidatorOptionsSectionName);
+        services.AddOptions<JwtValidatorOptions>().BindConfiguration(DefaultAppSettingsKeys.JwtValidator);
         services.AddScoped<IJwtValidatorSigningCredentialsCreationStrategy, TSigningCredentialsCreationStrategy>();
         services.AddScoped<IJwtValidatorService, JwtValidatorService>();
 
--- src/Sample.Api/Sample.Api.Public/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sample.Api.Public;
-public static class Constants
-{
-    public static class AppSettingsKeys
-    {
-        public const string JwtGenerator = "JwtGenerator";
-        public const string JwtValidator = "JwtValidator";
-    }
-}
