05dc9366f320f532af5e7163e32a9149178ad51b
chyla95 <chyla95@gmail.com>
2024-01-01 23:33:39 +0100

    Add RefreshTokenUtility

---
+++ src/Sample.Api.Authentication/Sample.Api.Authentication.Jwt/Sample.Api.Authentication.Common/Sample.Api.Authentication.Common/Utilities/RefreshTokenUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+namespace Sample.Api.Authentication.Common.Utilities;
+internal sealed class RefreshTokenUtility : IRefreshTokenUtility
+{
+    public string GenerateRefreshToken(int tokenLength = 32)
+    {
+        if(tokenLength < 32) throw new ArgumentException("Refresh token length must be at least 32 characters.", nameof(tokenLength));
+
+        using RandomNumberGenerator randomNumberGenerator = RandomNumberGenerator.Create();
+
+        byte[] randomNumber = new byte[tokenLength];
+        randomNumberGenerator.GetBytes(randomNumber);
+        string refreshToken = Convert.ToBase64String(randomNumber);
+
+        return refreshToken;
+    }
+}
