ff461438610ab849bfa585601eb1c5b219d72535
chyla95 <chyla95@gmail.com>
2024-01-01 17:38:00 +0100

    Fix AuthenticationController name

---
+++ src/Sample.Api/Sample.Api.Public/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sample.Api.Authentication.Jwt.Services;
+
+namespace Sample.Api.Public.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController(IJwtValidatorService jwtValidatorService, IJwtGeneratorService jwtGeneratorService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<bool>> Get(CancellationToken cancellationToken)
+    {
+        var token = await jwtGeneratorService.GenerateTokenAsync("123", cancellationToken);
+        var isTokenValid = await jwtValidatorService.IsTokenValidAsync(token, cancellationToken);
+        var canTokenBeRefreshed = await jwtValidatorService.CanTokenBeRefreshedAsync(token, cancellationToken);
+
+        return Ok(token);
+    }
+
+    [Authorize]
+    [HttpGet("NeedAuth")]
+    public ActionResult<bool> Get2()
+    {
+        return Ok(true);
+    }
+}
