bc5b5aff64e108e87512a3c8a1b372fc4120a75f
chyla95 <chyla95@gmail.com>
2024-01-13 16:08:33 +0100

    Reorganize constants

--- src/Sample.Application/Sample.Application.WebApi/Sample.Application.WebApi.Public/Constants/AppSettingsKeys.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Sample.Application.WebApi.Public.Constants;
-
-public static class AppSettingsKeys
-{
-    public const string SampleA = "...";
-    public const string SampleB = "...";
-}
