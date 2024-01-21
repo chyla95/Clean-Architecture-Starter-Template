8f23fd7fb42c90f55bb061718e40751962e425cd
chyla95 <chyla95@gmail.com>
2024-01-21 18:33:36 +0100

    Add mailing

---
+++ src/Sample.Architecture/Sample.Architecture.Application.Mailing/Enums/MailBodyType.cs
@@ -0,0 +1,14 @@
+﻿namespace Sample.Architecture.Application.Mailing.Enums;
+public enum MailBodyType
+{
+    /// <summary>
+    /// Represents the plain text format for the body of an email message. 
+    /// This type typically contains simple and unformatted textual content.
+    /// </summary>
+    Text,
+    /// <summary>
+    /// Represents the HTML format for the body of an email message. 
+    /// This type allows for richer and formatted content, including the use of HTML tags for styling and layout.
+    /// </summary>
+    Html
+}
---
+++ src/Sample.Architecture/Sample.Architecture.Application.Mailing/Options/MailSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿using Sample.Architecture.Application.Mailing.Enums;
+
+namespace Sample.Architecture.Application.Mailing.Options;
+
+public sealed record MailSenderOptions 
+{
+    public required MailSenderClientOptions DefaultMailSenderClientOptions { get; init; }
+    public required IEnumerable<MailSenderClientOptions> OtherMailSenderClientsOptions { get; init; }
+}
